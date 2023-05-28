04e2825afbd2f4b53028e78f36519b78683f6324
Andreh Silveira <andrehbahia@gmail.com>
2023-05-28 14:21:56 -0300

    inclui NotaInvalida

--- src/App.java
+++ src/App.java
@@ -1,14 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int nota = 65;
+        int nota = -65;
         if (nota>=80){
             System.out.println("Graduação A");
         }else if (nota<80 && nota>=70){
             System.out.println("Graduação B");
         }else if (nota<70 && nota>=60){
             System.out.println("Graduação C");
-        }else{
+        }else if (nota<60 && nota>=0){
             System.out.println("Graduação D");
+        }else{
+            System.out.println("Nota inválida");
         }
     }
 }
